50469d7a62119e01a31c6a53b36db61197d61adf
Tabasaran <1tabasaran@gmail.com>
2020-08-22 17:40:08 +0300

    Spawn birds
    Birds have randomly spawn position,  speed and scale.

---
+++ Assets/Scripts/BirdControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BirdControl : MonoBehaviour
+{
+
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1f, 5f);
+        transform.localScale *= Random.Range(0.4f, 1.2f);
+
+        if (transform.position.x > 0f)
+        {
+            //flip
+            transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+            speed *= -1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject yellowBird;
+    public GameObject pinkBird;
+
+    private Vector3 spawnPosition;
+
+    void Start()
+    {
+        StartCoroutine("SpawnObj");
+    }
+
+
+    IEnumerator SpawnObj()
+    {
+        spawnPosition.x = Random.Range(0, 2) == 1 ? -10: 10;
+        spawnPosition.y = Random.Range(-4.5f, 4.5f);
+
+        if (Random.Range(0, 2) == 1)
+            Instantiate(yellowBird, spawnPosition, Quaternion.identity);
+        else
+            Instantiate(pinkBird, spawnPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(Random.Range(.0f, 5.0f));
+        StartCoroutine("SpawnObj");
+    }
+}
