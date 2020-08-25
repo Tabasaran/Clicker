91442f99ba9e6282ba2ecad1529dd3abb5e4887c
Tabasaran <1tabasaran@gmail.com>
2020-08-25 17:30:45 +0300

    Added Quit button
    1.01v

---
+++ Assets/Scripts/MusicScaleController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicScaleController : MonoBehaviour
+{
+    private Slider musicScaleSlader;
+
+    void Start()
+    {
+        musicScaleSlader = GetComponent<Slider>();
+        musicScaleSlader.value = PlayerPrefs.GetFloat("musicScale", 1f);
+    }
+    public void ChangeMusicScale()
+    {
+        MusicManager.instance.musicScale = musicScaleSlader.value;
+        MusicManager.instance.audioSource.volume = musicScaleSlader.value;
+        PlayerPrefs.SetFloat("musicScale", musicScaleSlader.value);
+    }
+
+}
